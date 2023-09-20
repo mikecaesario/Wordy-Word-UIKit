324a1299a669f39e45ebc898d7de2b1c4dd50c01
Michael Caesario <michaelcaesario@live.com>
2023-09-20 23:39:30 +0700

    added KVO to WKWebView to observe canGoBack and canGoForward to reflect to the navigation button

--- Wordy Word (UIKit)/Screens/Settings/Controllers/WebViewController.swift
+++ Wordy Word (UIKit)/Screens/Settings/Controllers/WebViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 import WebKit
 
-final class WebViewController: UIViewController, WKUIDelegate {
+final class WebViewController: UIViewController {
 
-    private let navigationBar = UIView()
-    private let webView = WKWebView()
+    private let navigationBar = WebNavigation()
+    private var webView: WKWebView!
     
     private let url: URL
     
@@ -27,9 +27,7 @@ final class WebViewController: UIViewController, WKUIDelegate {
     override func loadView() {
         super.loadView()
         
-        webView.load(URLRequest(url: url))
-        webView.uiDelegate = self
-        webView.allowsBackForwardNavigationGestures = true
+        prepareWebView()
     }
     
     override func viewDidLoad() {
@@ -37,6 +35,25 @@ final class WebViewController: UIViewController, WKUIDelegate {
 
         configureView()
         layoutUI()
+//        goToWeb(withURL: url)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        goToWeb(withURL: url)
+    }
+    
+    deinit {
+        
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward))
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack))
+        print("DEINTILIZE WEBVIEW")
+    }
+    
+    private func goToWeb(withURL: URL) {
+        
+        let urlRequest = URLRequest(url: url)
+        webView.load(urlRequest)
     }
     
     private func setSheetDetentToMedium() {
@@ -48,8 +65,19 @@ final class WebViewController: UIViewController, WKUIDelegate {
         }
     }
     
-    private func dismissWebViewController() {
-        navigationController?.popViewController(animated: true)
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        switch keyPath {
+            
+        case #keyPath(WKWebView.canGoBack):
+            
+            navigationBar.isBackButtonEnabled(isEnabled: webView.canGoBack)
+        case #keyPath(WKWebView.canGoForward):
+            
+            navigationBar.isForwardButtonEnabled(isEnabled: webView.canGoForward)
+        default:
+            break
+        }
     }
 }
 
@@ -61,31 +89,179 @@ extension WebViewController {
         
         navigationController?.isNavigationBarHidden = true
         
+        navigationBar.delegate = self
+    }
+    
+    private func prepareWebView() {
+        
+        let webViewConfiguration = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
+        webView.uiDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        webView.frame = view.bounds
+        
+        view.addSubview(webView)
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
     }
     
     private func layoutUI() {
         
         navigationBar.translatesAutoresizingMaskIntoConstraints = false
-        webView.translatesAutoresizingMaskIntoConstraints = false
+//        webView.translatesAutoresizingMaskIntoConstraints = false
         
-        let views = [navigationBar, webView]
+//        let views = [navigationBar, webView]
         
-        view.addSubviews(views)
-        webView.layer.zPosition = -1
+        view.addSubview(navigationBar)
+        navigationBar.layer.zPosition = 1
+        view.bringSubviewToFront(navigationBar)
         
         NSLayoutConstraint.activate([
             
-            navigationBar.topAnchor.constraint(equalTo: view.topAnchor),
+            navigationBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            navigationBar.heightAnchor.constraint(equalToConstant: 100),
-
-            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            navigationBar.heightAnchor.constraint(equalToConstant: 75),
+//
+//            webView.topAnchor.constraint(equalTo: view.topAnchor),
+//            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 100),
+//            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 }
 
+extension WebViewController: WebNavigationDelegate, WKUIDelegate, WKNavigationDelegate {
+    
+    func didFinishTappingCloseButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func didFinishTappingBackButton() {
+        webView.goBack()
+    }
+    
+    func didFinishTappingForwardButton() {
+        webView.goForward()
+    }
+    
+}
+
+protocol WebNavigationDelegate: AnyObject {
+    
+    func didFinishTappingCloseButton()
+    func didFinishTappingBackButton()
+    func didFinishTappingForwardButton()
+}
+
+final class WebNavigation: UIView {
+    
+    private let gradientBackground = CAGradientLayer()
+    private let closeButton = NavigationBarCircleButton()
+    private let backButton = NavigationBarCircleButton()
+    private let forwardButton = NavigationBarCircleButton()
+
+    weak var delegate: WebNavigationDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        layoutUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        
+        self.backgroundColor = .clear
+        
+        closeButton.setImageForButton(imageName: "xmark", size: 22)
+        backButton.setImageForButton(imageName: "chevron.left", size: 22)
+        forwardButton.setImageForButton(imageName: "chevron.right", size: 22)
+        
+        backButton.tintColor = .text.grey
+        backButton.isEnabled = false
+        
+        forwardButton.tintColor = .text.grey
+        forwardButton.isEnabled = false
+        
+        closeButton.addTarget(self, action: #selector(didTappedBackButton), for: .touchUpInside)
+        backButton.addTarget(self, action: #selector(didTappedBackButton), for: .touchUpInside)
+        forwardButton.addTarget(self, action: #selector(didTappedForwardButton), for: .touchUpInside)
+    }
+    
+    private func layoutUI() {
+        
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        forwardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubviews([closeButton, backButton, forwardButton])
+        
+        let padding = 16.0
+        let forwardAndBackSpacing = 10.0
+        let multiplierValue = 0.8
+        
+        NSLayoutConstraint.activate([
+        
+            closeButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            closeButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            closeButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue),
+            closeButton.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue),
+            
+            forwardButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            forwardButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            forwardButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue),
+            forwardButton.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue),
+            
+            backButton.trailingAnchor.constraint(equalTo: forwardButton.leadingAnchor, constant: -forwardAndBackSpacing),
+            backButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            backButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue),
+            backButton.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: multiplierValue)
+
+        ])
+    }
+    
+    @objc private func didTappedCloseButton() {
+        print("CLOSE BUTTON TAPPED")
+        delegate?.didFinishTappingCloseButton()
+    }
+    
+    @objc private func didTappedBackButton() {
+        print("BACK BUTTON TAPPED")
+
+        delegate?.didFinishTappingBackButton()
+    }
+    
+    @objc private func didTappedForwardButton() {
+        print("FORWARD BUTTON TAPPED")
+
+        delegate?.didFinishTappingForwardButton()
+    }
+    
+    func isBackButtonEnabled(isEnabled: Bool) {
+        
+        if isEnabled {
+            backButton.tintColor = .text.white
+            backButton.isEnabled = true
+        } else {
+            backButton.tintColor = .text.grey
+            backButton.isEnabled = false
+        }
+    }
+    
+    func isForwardButtonEnabled(isEnabled: Bool) {
+        
+        if isEnabled {
+            forwardButton.tintColor = .text.white
+            forwardButton.isEnabled = true
+        } else {
+            forwardButton.tintColor = .text.grey
+            forwardButton.isEnabled = false
+        }
+    }
+}
 
--- Wordy Word (UIKit)/Services/TextEditorService.swift
+++ Wordy Word (UIKit)/Services/TextEditorService.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
-//protocol TextEditorServiceProtocol {
-//    func
-//}
-//
-//class TextEditorService: TextEditorServiceProtocol {
-//    
-//}
+protocol TextEditorServiceProtocol {
+    func startEditText(text: String?, editingStyle: EditingStyleEnum?, remove: [String]?, find: String?, replace: String?) -> String
+}
+
+class TextEditorService: TextEditorServiceProtocol {
+    func startEditText(text: String?, editingStyle: EditingStyleEnum?, remove: [String]?, find: String?, replace: String?) -> String {
+        return ""
+    }
+    
+   
+    
+    
+}
