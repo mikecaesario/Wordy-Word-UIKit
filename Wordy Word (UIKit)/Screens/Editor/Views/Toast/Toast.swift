71f15db7e249c0cf754a058d7b68c0fce0905a33
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-12-14 02:41:11 +0700

    final commit

---
+++ Wordy Word (UIKit)/Screens/Editor/Views/Toast/Toast.swift
@@ -0,0 +1,13 @@
+//  
+//  ToastView.swift
+//  Wordy Word (UIKit)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 11/12/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import Foundation
---
+++ Wordy Word (UIKit)/Utilities/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,13 @@
+//  
+//  UIViewController+Ext.swift
+//  Wordy Word (UIKit)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 12/12/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import Foundation
